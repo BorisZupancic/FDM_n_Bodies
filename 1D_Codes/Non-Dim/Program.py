8400e03473fc4a164d52231315f5c81179219941
BorisZ <18bz20@queensu.ca>
2022-06-07 15:05:53 -0400

    Folder Name Change

--- 1D_Codes/Non-Dim/Program.py
+++ 1D_Codes/Non-Dim/Program.py
@@ -18,7 +18,7 @@ import OneD.GlobalFuncs as GF
 #Set up Directory for saving files/images/videos
 # Will not rename this again
 from pathlib import Path
-Directory = os.getcwd()#"/1D codes/Non-Dim" #os.curdir() #"/home/boris/Documents/Research/Coding/1D codes/Non-Dim"
+Directory = os.getcwd()+"/1D codes/Non-Dim" #os.curdir() #"/home/boris/Documents/Research/Coding/1D codes/Non-Dim"
 print(Directory)
 ############################################
 # SET UP: shared by Wave and N-Body scenarios
