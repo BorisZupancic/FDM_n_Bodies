00ed0ffc210c907fa3b3058c7a086acd4104e66d
BorisZ <18bz20@queensu.ca>
2022-06-07 17:22:53 -0400

    Update ProgramTesting.sh

--- 1D_Codes/Non-Dim/ProgramTesting.sh
+++ 1D_Codes/Non-Dim/ProgramTesting.sh
@@ -1,3 +1,11 @@
 cd "/home/boris/Documents/Research/Coding"
 
-printf "2\n3\n1\n10000\n1\n10000\n1\n0.1\n0.1" | python3 -u 1D_Codes/Non-Dim/Program.py 
+masses=('2' '1' '0.5' '0.1' '0.05' '0.01')
+simulation_type=('1' '2')
+for m in "${masses[@]}"; do
+    # printf $m
+    for sim in simulation_type; do
+        printf '%f\n%i\n%f\n%i\n%f\n%i\n%i\n%f\n%f' "2" "3" "$m" "10000" "1" "10000" "$sim" "0.1" "0.1"
+        printf '%f\n%i\n%f\n%i\n%f\n%i\n%i\n%f\n%f' "2" "3" "$m" "10000" "1" "10000" "$sim" "0.1" "0.1" | python3 -u 1D_Codes/Non-Dim/Program.py 
+    done
+done
