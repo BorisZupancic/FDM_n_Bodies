deedaaa9bad3eef65744b10fc7e59c0ab14a9aeb
BorisZ <18bz20@queensu.ca>
2022-06-07 18:08:53 -0400

    Update ProgramTesting.sh

--- 1D_Codes/Non-Dim/ProgramTesting.sh
+++ 1D_Codes/Non-Dim/ProgramTesting.sh
@@ -1,11 +1,12 @@
 cd "/home/boris/Documents/Research/Coding"
 
+num_particles='10000'
 masses=('2' '1' '0.5' '0.1' '0.05' '0.01')
 simulation_choice=('1' '2')
 for m in "${masses[@]}"; do
     # printf $m
     for sim in "${simulation_choice[@]}"; do
-        printf '%f\n%i\n%f\n%i\n%f\n%i\n%i\n%f\n%f' "2" "3" "$m" "10000" "1" "10000" "$sim" "0.1" "0.1"
+        printf '%f\n%i\n%f\n%i\n%f\n%i\n%i\n%f\n%f' "2" "3" "$m" "$num_particles" "1" "$num_particles" "$sim" "0.1" "0.1"
         printf '%f\n%i\n%f\n%i\n%f\n%i\n%i\n%f\n%f' "2" "3" "$m" "10000" "1" "10000" "$sim" "0.1" "0.1" | python3 -u 1D_Codes/Non-Dim/Program.py 
     done
 done
