33fb924b3f50d86ea7463785d269b3320c6a5c26
BorisZ <18bz20@queensu.ca>
2022-06-09 18:02:28 -0400

    Update Testing.sh

--- 1D_Codes/Non-Dim/Program/Testing.sh
+++ 1D_Codes/Non-Dim/Program/Testing.sh
@@ -1,28 +1,33 @@
 cd "/home/boris/Documents/Research/Coding"
 
 #Possible choices for number of Bosons and/or Particles:
-num_bosons=('0' '1') #'10000')
+num_bosons=('0') #'10000')
 num_particles=('0' '10000')
 
 #Choices of either full simulation video or snapshots:
 simulation_choice2=('1' '2')
+
+# for num_b in "${num_bosons[@]}"; do
+# echo "$num_b"
+# done
 
 for num_b in "${num_bosons[@]}"; do
 for num_p in "${num_particles[@]}"; do
     #Break loop if num_b and num_p are both zero
     if [ "$num_b" == '0' ]; then
         if [ "$num_p" == "0" ]; then
-            break 
+            continue #go to next step of loop
         fi
     fi
 
+    masses=('1' '1')
     #Set Boson Mass array, depending on Number of Bosons
     if [ "$num_b" != '0' ]; then
         masses=('2' '1' '0.5' '0.1' '0.05' '0.04' '0.03' '0.02' '0.01')
-    else
-        masses=('1') #Default, won't matter in simulation
+    #else
+     #   masses=('1') #Default, won't matter in simulation
     fi
-echo "$num_b"
+
 #Now run simulation for given num_b, num_p and masses
 for m in "${masses[@]}"; do
     # printf $m
